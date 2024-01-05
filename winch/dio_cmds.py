1f9fc31ec4fc0b1dc126536db408a0e7bc9c052e
danauerbach <dauerbach@ucsd.edu>
2024-01-04 21:49:08 -0800

    cleanup get_latch... and get_payout... funcs so can be called during simulation

--- winch/dio_cmds.py
+++ winch/dio_cmds.py
@@ -29,12 +29,12 @@ from . import  WinchDir
 
 class DIOCommander():
 
-    def __init__(self, cfg: dict, simulation: bool = True):
+    def __init__(self, cfg: dict):
         self.cfg = cfg
         self.dio_tty_port = cfg["rift-ox-pi"]["DIO_PORT"]
-        self.simulation = simulation
+        self.simulation = cfg["rift-ox-pi"]["SIMULATION"]
 
-        print(f"SIMULATION: {simulation}")
+        print(f"SIMULATION: {self.simulation}")
 
         self.MOTOR_STOP_PIN = {
             "group": cfg["rift-ox-pi"]["DIO_MOTOR_STOP_GROUP"],
@@ -191,67 +191,74 @@ class DIOCommander():
         cmd = f'dio edge DI_G{self.LATCH_SENSOR_PIN["group"]} {self.LATCH_SENSOR_PIN["pin"]}\r'
         edge_cnt_str, err = self.issue_command(cmd=cmd)
         if err:
-            return "0-", True
+            return "0", True
         return edge_cnt_str, False
 
     def get_payout_edge_count(self) -> Tuple[list[int], bool]:
-        payout_1: str
-        payout_2: str
+        payout_1: str = ''
+        payout_2: str = ''
+        p1: int = 0
+        p2: int = 0
+        res: bool = False
         cmd = f'dio edge DI_G{self.PAYOUT1_PIN["group"]} {self.PAYOUT1_PIN["pin"]}\r'
         payout_1, err = self.issue_command(cmd=cmd)
-        if err:
-            return [], True
-        if payout_1 != None:
+        if not err and payout_1.isdigit():
             p1 = int(payout_1)
+        else:
+            res = True
 
         cmd = f'dio edge DI_G{self.PAYOUT2_PIN["group"]} {self.PAYOUT2_PIN["pin"]}\r'
         payout_2, err = self.issue_command(cmd=cmd)
-        if err:
-            return [],  True
-        if payout_2 != None:
+        if not err and payout_2.isdigit():
             p2 = int(payout_2)
+        else:
+            res = True
 
-        return [p1, p2], False
+        return [p1, p2], res
 
     def get_winch_direction(self) -> Tuple[str, bool]:
         err: bool = False
-        up_active: bool
-        down_active: bool
-        stop_active: bool
+        res: bool = False
+        up_active: bool = False
+        down_active: bool = False
+        stop_active: bool = False
         up_pin_query = f'dio get DO_G{self.UPCAST_PIN["group"]} output {self.UPCAST_PIN["pin"]}\r'
         down_pin_query = f'dio get DO_G{self.DOWNCAST_PIN["group"]} output {self.DOWNCAST_PIN["pin"]}\r'
         stop_pin_query = f'dio get DO_G{self.MOTOR_STOP_PIN["group"]} output {self.MOTOR_STOP_PIN["pin"]}\r'
 
         up_pin_state, err = self.issue_command(up_pin_query)
-        if err:
-            return WinchDir.DIRECTION_NONE.value, True
-        if up_pin_state != None:
+        if not err and up_pin_state.isdigit():
             up_active = not bool(int(up_pin_state))  # "1" is active but in python 0 == True
+        else:
+            res = True
 
         down_pin_state, err = self.issue_command(down_pin_query)
-        if err:
-            return WinchDir.DIRECTION_NONE.value, True
-        if down_pin_state != None:
+        if not err and down_pin_state.isdigit():
             down_active = not bool(int(down_pin_state))
+        else:
+            res = True
 
         stop_pin_state, err = self.issue_command(stop_pin_query)
-        if err:
-            return WinchDir.DIRECTION_NONE.value, True
-        if stop_pin_query != None:
+        if not err and stop_pin_state.isdigit():
             stop_active = not bool(int(stop_pin_state))
+        else:
+            res = True
 
-        stop_active = stop_pin_state == "1"
-        down_active = down_pin_state == "1"
-        up_active = up_pin_state == "1"
+        if res:
+            return WinchDir.DIRECTION_NONE.value, res
+
+        # stop_active = stop_pin_state == "1"
+        # down_active = down_pin_state == "1"
+        # up_active = up_pin_state == "1"
         if stop_active:
-            return WinchDir.DIRECTION_NONE.value, False
+            return WinchDir.DIRECTION_NONE.value, res
 
         # stop not active...
-        if up_active and not down_active: return WinchDir.DIRECTION_UP.value, err
-        if not up_active and down_active: return WinchDir.DIRECTION_DOWN.value, err
+        if up_active and not down_active: return WinchDir.DIRECTION_UP.value, res
+        if not up_active and down_active: return WinchDir.DIRECTION_DOWN.value, res
         
         # either both direction lines HIGH or both LOW, either way winch not moving.
-        return WinchDir.DIRECTION_NONE.value, err
+        return WinchDir.DIRECTION_NONE.value, res
 
     def issue_command(self, cmd : str) -> Tuple[str, bool]:
 
@@ -386,8 +393,6 @@ class DIOCommander():
                 # cmd_bytes = dio_command_bytes(DIO_ACTION_NUMINPUTS_NAME, dir=DIO_DIRECTION_IN, group=0, pin=1)
                 written = mcu.write(cmd_bytes)
                 mcu.flush()
-                print(f'_send_bytes: Command sent: {cmd_bytes.decode().strip()}')
-                #TODO LOG INFO
 
                 time.sleep(0.01)
                 res = mcu.read(mcu.in_waiting)
