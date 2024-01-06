7908a051d7c137f3ee436a04ee575132acab931e
danauerbach <dauerbach@ucsd.edu>
2024-01-06 14:22:21 -0800

    use input instead of readline

--- roloc.py
+++ roloc.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2018 Brent Rubell for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
 
-"""
-Wiring Check, Pi Radio w/RFM9x
-
-Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
-Author: Brent Rubell for Adafruit Industries
-"""
+import signal
 import sys
 import time
 import busio
@@ -22,86 +14,106 @@ import adafruit_rfm9x
 from winch import WinchCmd
 CMD_QUIT = "QUIT"
 
-# Button A
-btnA = DigitalInOut(board.D5)
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
 
-# Button B
-btnB = DigitalInOut(board.D6)
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
+def interrupt_handler(signum, frame):
 
-# Button C
-btnC = DigitalInOut(board.D12)
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
-
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# 128x32 OLED Display
-reset_pin = DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-# Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
-
-# Configure RFM9x LoRa Radio
-CS = DigitalInOut(board.CE1)
-RESET = DigitalInOut(board.D25)
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-while True:
-    # Clear the image
     display.fill(0)
-
-    # Attempt to set up the RFM9x Module
-    try:
-        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-        display.text('RFM9x: Detected', 0, 0, 1)
-    except RuntimeError as error:
-        # Thrown on version mismatch
-        display.text('RFM9x: ERROR', 0, 0, 1)
-        print('RFM9x Error: ', error)
-
-    # check for a packet
-#    packet = rfm9x.receive(timeout=1.0)
-
-#    if packet is None:
-#      # Packet has not been received
-#        display.text('no pck rcvd', 0, height-24, 1)
-#    else:
-#        display.text(packet.decode('utf-8'))
-#    display.show()
-        
-    print(f'ENTER RIFT-OX CMD: ', end='')
-    cmd: str = sys.stdin.readline()
-    while cmd.upper() != CMD_QUIT:
-        print(f'ENTER RIFT-OX CMD: ', end='')
-        cmd = sys.stdin.readline()
+    display.text('roloc: shutting down', 0, 0, 1)
 
     sys.exit(0)
 
-    # Check buttons
-    if not btnA.value:
-        # Button A Pressed
-        display.text('Ada', width-85, height-7, 1)
-        display.show()
-        rfm9x.send(bytes("Button A Pressed", "utf-8"))
-        time.sleep(0.1)
-    if not btnB.value:
-        # Button B Pressed
-        display.text('Fruit', width-75, height-7, 1)
-        display.show()
-        time.sleep(0.1)
-    if not btnC.value:
-        # Button C Pressed
-        display.text('Radio', width-65, height-7, 1)
+
+def main():
+
+    while True:
+        # Clear the image
+        display.fill(0)
+
+        # Attempt to set up the RFM9x Module
+        try:
+            rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+            display.text('RFM9x: Detected', 0, 0, 1)
+            print('RFM9x: Detected')
+        except RuntimeError as error:
+            # Thrown on version mismatch
+            display.text('RFM9x: ERROR', 0, 0, 1)
+            print('RFM9x Error: ', error)
+
+        # check for a packet
+    #    packet = rfm9x.receive(timeout=1.0)
+
+    #    if packet is None:
+    #      # Packet has not been received
+    #        display.text('no pck rcvd', 0, height-24, 1)
+    #    else:
+    #        display.text(packet.decode('utf-8'))
+    #    display.show()
+            
+        cmd = input(f'ENTER RIFT-OX CMD: ')
+        while cmd.upper() != CMD_QUIT:
+            cmd = input(f'ENTER RIFT-OX CMD: ')
+
+        display.fill(0)
+        display.text('roloc: shutting down', 0, 0, 1)
+        sys.exit(0)
+
+
+        # Check buttons
+        if not btnA.value:
+            # Button A Pressed
+            display.text('Ada', width-85, height-7, 1)
+            display.show()
+            rfm9x.send(bytes("Button A Pressed", "utf-8"))
+            time.sleep(0.1)
+        if not btnB.value:
+            # Button B Pressed
+            display.text('Fruit', width-75, height-7, 1)
+            display.show()
+            time.sleep(0.1)
+        if not btnC.value:
+            # Button C Pressed
+            display.text('Radio', width-65, height-7, 1)
+            display.show()
+            time.sleep(0.1)
+
         display.show()
         time.sleep(0.1)
 
+if __name__ == "__main__":
+    
+    signal.signal(signal.SIGINT, interrupt_handler)
+
+    # Button A
+    btnA = DigitalInOut(board.D5)
+    btnA.direction = Direction.INPUT
+    btnA.pull = Pull.UP
+
+    # Button B
+    btnB = DigitalInOut(board.D6)
+    btnB.direction = Direction.INPUT
+    btnB.pull = Pull.UP
+
+    # Button C
+    btnC = DigitalInOut(board.D12)
+    btnC.direction = Direction.INPUT
+    btnC.pull = Pull.UP
+
+    # Create the I2C interface.
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # 128x32 OLED Display
+    reset_pin = DigitalInOut(board.D4)
+    display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+    # Clear the display.
+    display.fill(0)
     display.show()
-    time.sleep(0.1)
+    width = display.width
+    height = display.height
+
+    # Configure RFM9x LoRa Radio
+    CS = DigitalInOut(board.CE1)
+    RESET = DigitalInOut(board.D25)
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+    main()
+    
