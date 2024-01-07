806da05b0e373fe9fb3d593ef9692b22e5c5f8c2
danauerbach <dauerbach@ucsd.edu>
2024-01-06 22:38:30 -0800

    stripped down to simple loop checking for received LoRa data

--- rolod.py
+++ rolod.py
@@ -1,98 +1,68 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2018 Brent Rubell for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
 
-"""
-Wiring Check, Pi Radio w/RFM9x
-
-Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
-Author: Brent Rubell for Adafruit Industries
-"""
+import signal
 import sys
 import time
 import busio
-from digitalio import DigitalInOut, Direction, Pull
+from digitalio import DigitalInOut
 import board
 # Import the SSD1306 module.
 import adafruit_ssd1306
 # Import the RFM9x radio module.
 import adafruit_rfm9x
 
-# Button A
-btnA = DigitalInOut(board.D5)
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
 
-# Button B
-btnB = DigitalInOut(board.D6)
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
+def main():
 
-# Button C
-btnC = DigitalInOut(board.D12)
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
+    def interrupt_handler(signum, frame):
 
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
+        sys.exit(0)
 
-# 128x32 OLED Display
-reset_pin = DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-# Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
+    signal.signal(signal.SIGINT, interrupt_handler)
 
-# Configure RFM9x LoRa Radio
-CS = DigitalInOut(board.CE1)
-RESET = DigitalInOut(board.D25)
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-while True:
+    # Create the I2C interface.
+    i2c = busio.I2C(board.SCL, board.SDA)
 
-    # Clear the image
+    # 128x32 OLED Display
+    reset_pin = DigitalInOut(board.D4)
+    display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+    # Clear the display.
     display.fill(0)
-
-    # Attempt to set up the RFM9x Module
-    try:
-        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-        display.text('RFM9x: Detected', 0, 0, 1)
-    except RuntimeError as error:
-        # Thrown on version mismatch
-        display.text('RFM9x: ERROR', 0, 0, 1)
-        print(f'RFM9x Error: {error}. Quitting.')
-        sys.exit(1)
-
-    # check for a packet
-    packet = rfm9x.receive(timeout=1.0)
-
-    if packet is None:
-      # Packet has not been received
-        display.text('no pck rcvd', 0, height-24, 1)
-    else:
-        print(f'cmd rcvd: {packet.decode()}')
-        display.text(f'cmd rcvd: {packet.decode()}', 0, height-18, 1)
     display.show()
+    width = display.width
+    height = display.height
 
-#     # Check buttons
-#     if not btnA.value:
-#         # Button A Pressed
-#         display.text('Ada', width-85, height-7, 1)
-#         display.show()
-# #        rfm9x.send(bytes("Button A Pressed\r\n", "utf-8"))
-#         time.sleep(0.1)
-#     if not btnB.value:
-#         # Button B Pressed
-#         display.text('Fruit', width-75, height-7, 1)
-#         display.show()
-#         time.sleep(0.1)
-#     if not btnC.value:
-#         # Button C Pressed
-#         display.text('Radio', width-65, height-7, 1)
-#         display.show()
-#         time.sleep(0.1)
+    # Configure RFM9x LoRa Radio
+    CS = DigitalInOut(board.CE1)
+    RESET = DigitalInOut(board.D25)
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-    time.sleep(0.1)
+    while True:
+
+        # Clear the image
+        display.fill(0)
+
+        # Attempt to set up the RFM9x Module
+        try:
+            rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+            print('RFM9x: Detected')
+        except RuntimeError as error:
+            # Thrown on version mismatch
+            print(f'RFM9x Error: {error}. Quitting.')
+            sys.exit(1)
+
+        # check for a packet
+        packet = rfm9x.receive(timeout=0.5, with_ack=True)
+
+        if packet is None:
+        # Packet has not been received
+            display.text('Nothing rcvd', 0, height-10, 1)
+        else:
+            packet_str = packet.decode()
+            print(f'rcvd: {packet_str}')
+            display.text(f'rcvd: {packet_str}', 0, height-20, 1)
+
+        display.show()
+
+        time.sleep(0.1)
