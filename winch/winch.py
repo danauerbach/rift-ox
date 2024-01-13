4bb59a99a2b51550cceac55155121da743d2ec7f
danauerbach <dauerbach@ucsd.edu>
2024-01-12 21:53:36 -0800

    add latch_release to START in ParkedState; add sleeps in START sequence

--- winch/winch.py
+++ winch/winch.py
@@ -90,8 +90,13 @@ class ParkedState():
 
     def start(self):
         self.winch.cmndr.up_cast(stop_after_ms=500)
+        time.sleep(3)
         self.winch.cmndr.latch_hold()
+        time.sleep(3)
         self.winch.cmndr.down_cast(stop_after_ms=1000)
+        time.sleep(3)
+        self.winch.cmndr.latch_release()
+        time.sleep(3)
         self.winch.cmndr.stage()
         self.winch.set_state(StagingState(self.winch))
 
