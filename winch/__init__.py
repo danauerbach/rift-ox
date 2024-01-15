ee4b0c78532bd2363dc8ce53a4505425d4fd4602
danauerbach <dauerbach@ucsd.edu>
2024-01-14 18:58:35 -0800

    refactor pub_cmd method name

--- winch/__init__.py
+++ winch/__init__.py
@@ -87,7 +87,7 @@ WINCH_CMD_LIST = {
 # DIO_VALID_MODES               = [DIO_MODE_DRAIN,
 #                                  DIO_MODE_SOURCE]
 
-def pub_winch_cmd(pubber: mqtt.Client, topic: str, command: str, **kwargs) -> bool:
+def pub_cmd(pubber: mqtt.Client, topic: str, command: str, **kwargs) -> bool:
 
     if command.upper() == 'GOSCIENCE':
         command = 'START'
